9933cdb3fe059ab31cb7ea69568ceec45bb1556b
Baolin <boringlee24@gmail.com>
2023-10-30 22:09:08 +0000

    Write Timings to a File

--- inference_tests/inference.py
+++ inference_tests/inference.py
@@ -2,6 +2,7 @@ import fire
 import os
 import sys
 import time
+import json
 
 import torch
 from transformers import LlamaTokenizer
@@ -14,7 +15,7 @@ def main(
     model_name,
     prompts,
     size: int=1000,
-    print_output: bool=False,
+    print_outputs: bool=False,
     print_times: bool=False,
     seed=0,
     peft_model=None,
@@ -30,8 +31,13 @@ def main(
     top_k: int=50,
     repetition_penalty: float=1.0, #The parameter for repetition penalty. 1.0 means no penalty.
     length_penalty: int=1, 
-   **kwargs
+    output_file: bool=None,
+    **kwargs
 ):
+    if not output_file:
+        print("No Output File Specified")
+        return
+
     if prompts is not None:
         assert os.path.exists(prompts), f"Provided Prompts do not exist: {prompts}"
   
@@ -60,6 +66,7 @@ def main(
     tokenizer.pad_token = tokenizer.eos_token
 
     e2e_inference_times = []
+    zipped = {}
 
     for instruction in randomInstructions:
         batch = tokenizer(instruction, padding='max_length', truncation=True, max_length=max_padding_length, return_tensors="pt")
@@ -81,14 +88,22 @@ def main(
             )
         e2e_inference_time = (time.perf_counter()-start)*1000
         e2e_inference_times.append(e2e_inference_time)
+        zipped[str(instruction)] = e2e_inference_time
 
         if print_times:
             print(f"Inference time: {e2e_inference_time}")
-        if print_output:
+        if print_outputs:
             output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
             print(f"Model output:\n{output_text}")
+        
     average_time = sum(e2e_inference_times) / len(e2e_inference_times)
     print(f"The average end-to-end inference time over {size} prompts is {average_time}")
+    
 
-if __name__ == "main":
-    fire.Fire(main)
+    json_string = json.dumps(zipped)
+
+    with open(output_file, 'w') as f:
+        json.dump(json_string, f)
+
+    
+fire.Fire(main)
